329315ed83631fb1bbf259113e2b85fbb37f236f
Yorgos Chatziparaskevas <georgio13@users.noreply.github.com>
2024-01-18 04:40:35 +0200

    Update project structure

--- demo/src/main/java/com/example/demo/booking/BookingRepository.java
+++ demo/src/main/java/com/example/demo/booking/BookingRepository.java
@@ -1,8 +1,6 @@
 package com.example.demo.booking;
 
-
-import com.example.demo.booking.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookingRepository extends JpaRepository<Booking, Long>{
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
--- demo/src/main/java/com/example/demo/trip/TripRepository.java
+++ demo/src/main/java/com/example/demo/trip/TripRepository.java
@@ -2,6 +2,5 @@ package com.example.demo.trip;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface TripRepository extends JpaRepository<Trip, Long> {
 }
