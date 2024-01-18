0e8f49c230971aaa2be30614a632ab3796966a35
Yorgos Chatziparaskevas <georgio13@users.noreply.github.com>
2024-01-18 17:00:18 +0200

    Add jwt

--- demo/src/main/java/com/example/demo/booking/BookingController.java
+++ demo/src/main/java/com/example/demo/booking/BookingController.java
@@ -1,12 +1,13 @@
 package com.example.demo.booking;
 
+import com.example.demo.config.InfoResponse;
 import com.example.demo.user.User;
 import com.example.demo.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -18,20 +19,26 @@ public class BookingController {
     @Autowired
     private UserService userService;
 
-    @PostMapping(path="/{afm}")
-    public void bookTrip(@PathVariable String afm, @RequestParam Long tripID, @RequestParam int numOfPeopleBooked) {
-        boolean bookingResult = bookingService.bookTrip(tripID, afm, numOfPeopleBooked);
-        if (bookingResult) {
-            System.out.println("Trip booked successfully");
-        } else {
-            System.out.println("Booking failed. No available seats or trip not found.");
+    @PostMapping(path="/")
+    public ResponseEntity<?> bookTrip(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestParam Long tripID, @RequestParam int numOfPeopleBooked) {
+        User user = userService.getUserFromToken(authorizationHeader);
+        if (user != null && user.getUserType().equals("citizen")) {
+            boolean bookingResult = bookingService.bookTrip(tripID, user.getAfm(), numOfPeopleBooked);
+            if (bookingResult) {
+                return ResponseEntity.ok().body(new InfoResponse("Ολοκληρώθηκε με επιτυχία"));
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new InfoResponse("Δεν υπάρχουν διαθέσιμες θέσεις"));
+            }
         }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new InfoResponse("Απαγορεύτηκε η είσοδος"));
     }
 
-    // Get citizen's booked trips
     @GetMapping(path = "/bookedTrips")
-    public List<Booking> myBookedTrips(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
+    public ResponseEntity<?> myBookedTrips(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
         User user = userService.getUserFromToken(authorizationHeader);
-        return bookingService.getMyBookings(user.getAfm());
+        if (user != null && user.getUserType().equals("citizen")) {
+            return ResponseEntity.ok(bookingService.getMyBookings(user.getAfm()));
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new InfoResponse("Απαγορεύτηκε η είσοδος"));
     }
 }
